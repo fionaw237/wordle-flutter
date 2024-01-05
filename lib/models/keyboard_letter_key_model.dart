6e116bfc2f8e8ef50bb7f3e235125207f6f2ffe6
Fiona Wilson <fionaw237@gmail.com>
2024-01-05 16:11:24 +0000

    Adding logic for setting colours of keyboard letter keys

--- lib/models/keyboard_letter_key_model.dart
+++ lib/models/keyboard_letter_key_model.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
 class KeyboardLetterKeyModel {
-  final VoidCallback onPress;
-  final String value;
-  final bool isDisabled;
-  final Color backgroundColour;
-
+  VoidCallback onPress;
+  String value;
+  bool isDisabled;
+  Color backgroundColour;
   KeyboardLetterKeyModel({required this.onPress, required this.value, required this.isDisabled, required this.backgroundColour});
 }
